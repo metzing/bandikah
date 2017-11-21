5a3217032ed00bb104adc470abc12b08e8146e3e
Metzing Márton <marton.metzing@gmail.com>
2017-11-22 00:00:00 +0100

    remove try catch

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -3,10 +3,6 @@ package assignment;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            GameTable gameTable = new GameTable(10, 10, 10);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        GameTable gameTable = new GameTable(10, 10, 10);
     }
 }
